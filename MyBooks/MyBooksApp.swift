11d581a3b8c4c44609487255452d4f1b6d777e8e
joaobug <joaoenfrance@gmail.com>
2024-09-01 13:25:43 -0300

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Joao Leal on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
